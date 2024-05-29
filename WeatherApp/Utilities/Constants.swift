6c83d9184a8ed1b9caae6a6d2dfeb99ccadba99d
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-29 20:51:54 +0200

    Implementing the endpoints so that we can generate the URL in the endpoints method that will combine the base url and the endpoints.

---
+++ WeatherApp/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Buhle Radzilani on 2024/05/29.
+//
+
+import Foundation
+
+struct Constants{
+    struct Keys {
+        static let weatherAPIKey = "c31995c03f3d7e286c564d973be5ac87"
+    }
+}
