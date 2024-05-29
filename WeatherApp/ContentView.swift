f9a1d32349628e7323d4ce5f94ce55ea6509a846
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-29 19:59:28 +0200

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Buhle Radzilani on 2024/05/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
