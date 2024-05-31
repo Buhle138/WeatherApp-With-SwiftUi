59f1782353b5f62d74e04535f6b26c918ed7869c
buhleradzilani <radzilanibuhle@gmail.com>
2024-05-31 22:27:42 +0200

    Overriding the Measurement extensions in order to get the actual temperature that we want.

--- WeatherApp/ContentView.swift
+++ WeatherApp/ContentView.swift
@@ -8,16 +8,44 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var city: String = ""
+    @State private var isFetchingWeather: Bool = false
+    @State private var weather: Weather?
+    
+    let geocodingClient = GeocodingClient()
+    let weatherClient = WeatherClient()
+    
+    private func fetchWeather() async {
+        do {
+            guard let location = try await geocodingClient.coordinateByCity(city: city) else {return}
+           weather = try await weatherClient.fetchWeather(location: location)
+        }catch{
+            print(error)
+        }
+      
+        
+    }
+    
     var body: some View {
         VStack {
-            Button("Get Coordinates"){
-                Task{
-                    let geocodingClient = GeocodingClient()
-                    let weatherClient = WeatherClient()
-                    let location = try! await geocodingClient.coordinateByCity(city: "Pretoria")
-                    let weather = try! await  weatherClient.fetchWeather(location: location!)
+            TextField("City", text: $city)
+                .textFieldStyle(.roundedBorder)
+                .onSubmit {
+                    isFetchingWeather = true
+                }.task(id: isFetchingWeather) {
+                    if isFetchingWeather{
+                        await fetchWeather()
+                        isFetchingWeather = false
+                        city = ""
+                    }
                 }
+            
+            if let weather {
+                Text("\(MeasurementFormatter.temperature(value: weather.temp))")
+                    .font(.system(size: 100))
             }
+            Spacer()
         }
         .padding()
     }
---
+++ WeatherApp/Extensions/MeasurementFormatter+Extensions.swift
@@ -0,0 +1,24 @@
+//
+//  MeasurementFormatter+Extensions.swift
+//  WeatherApp
+//
+//  Created by Buhle Radzilani on 2024/05/31.
+//
+
+import Foundation
+
+
+extension MeasurementFormatter {
+    static func temperature(value: Double) -> String {
+        let numberFormatter = NumberFormatter()
+        numberFormatter.maximumFractionDigits = 0
+        
+        
+        let formatter = MeasurementFormatter()
+        formatter.numberFormatter = numberFormatter
+        
+        let temp = Measurement(value: value, unit: UnitTemperature.kelvin)
+        
+        return formatter.string(from: temp)
+    }
+}
